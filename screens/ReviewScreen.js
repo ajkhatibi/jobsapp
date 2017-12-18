a2872a456773e83c923466b2ae9538316f183afe
Julian Khatibi <ajkhatibi@gmail.com>
2017-12-18 10:17:12 -0800

    navigate to settings screen

--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import {View, Text} from 'react-native';
+import { Button } from 'react-native-elements';
 
 export default class ReviewScreen extends Component {
+    static navigationOptions = ({ navigation }) => ({
+    title: 'Review Jobs',
+    headerRight: <Button title='Settings' onPress={() => navigation.navigate('setting')} />
+    });
     render() {
         return (
             <View>
